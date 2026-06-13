07871d2d2af17b32952f3aa1fa4b983a6de097f1
vdeepikaa24 <your_email@example.com>
2026-06-13 14:11:40 +0530

    Exercise-1: Singleton Pattern

---
+++ src/main/engineeringConcepts/designPatternsAndPrinciples/SingletonPatternExample/Logger.java
@@ -0,0 +1,20 @@
+package main.engineeringConcepts.designPatternsAndPrinciples.SingletonPatternExample;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger instance create");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
